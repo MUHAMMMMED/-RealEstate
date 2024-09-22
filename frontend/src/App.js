e141b1bdc2f49547f9270ba7f2fc7c8ade5618b7
MUHAMMMMED <amer66965@gmail.com>
2024-09-22 16:55:55 +0300

    creat card

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
+
+import Card from './components/Card/Card';
+import Filter from './components/Filter/Filter';
 import ThemeSwitcher from './components/ThemeSwitcher/ThemeSwitcher';
 import './styles/global.css';
 import './styles/variables.css';
@@ -21,9 +24,8 @@ const App = () => {
   return (
     <div className={darkMode ? 'App dark' : 'App light'}>
       <Header darkMode={darkMode} />
-
-      <h1>Welcome to My Project</h1>
-
+      <Filter darkMode={darkMode} />
+      <Card darkMode={darkMode} />
       <ThemeSwitcher toggleTheme={toggleDarkMode} darkMode={darkMode} />
 
     </div>
